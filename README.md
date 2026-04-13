62ba1606c146916acc002194be0e49e143008ee0
08SjoAnt <anton.sjogren@skola.taby.se>
2026-04-13 11:02:48 +0200

    Lade till grundläggande kod
    Co-authored-by: ludvigstenlund <ludvigstenlund@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-README.md
+# The Legend of Poopfly 2: Nu med grafik!
+
+The Legend of Poopfly 2 är ett rougaktigt spel som är en vidareutvekling av [The Legend of Poopfly](https://github.com/liziarutinov/The-Legend-Of-Poopfly).
---
+++ script.js
@@ -0,0 +1,100 @@
+const canvas = document.getElementById("spelyta")
+const ctx = canvas.getContext("2d")
+
+let spritesheet = new Image()
+
+
+const spritewidth = 32
+const spriteheight = 32
+
+let frameIndex = 0
+let framestotal = 4
+
+let lastTimestamp = 0,x 
+    maxFPS = 15,
+    timestep = 1000 / maxFPS
+
+//how to walk
+
+
+
+class character {
+    constructor(name, speed, fire_rate, damage, xpos, ypos) {
+        this.name = name;
+        this.speed = speed;
+        this.fire_rate = fire_rate;
+        this.damage = damage;
+        this.xpos = xpos;
+        this.ypos = ypos;
+    }
+}
+
+let sp1 = new character("Standardkaraktär", 20, 2, 3, 0, 200)
+
+let keys = {
+    w: false,
+    s: false,
+    d: false,
+    a: false,
+}
+
+document.onkeydown = function(e) {
+    console.log(e)
+    const key = e.key
+    keys[key] = true
+}
+
+document.onkeyup = function(e) {
+    console.log(e)
+    const key = e.key
+    keys[key] = false
+}
+
+
+
+function draw(timestamp) {
+    if (timestamp - lastTimestamp < timestep) {
+        // Vi ska vänta med att rita så vi avbryter funktionen.
+        requestAnimationFrame(draw)
+        return  
+    }
+
+    lastTimestamp = timestamp
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height,)
+
+
+    if (keys.a || keys.d || keys.w || keys.s) {
+        spritesheet.src = "Dude_Monster_Walk_6.png"
+        framestotal = 6
+    }
+    else {
+        spritesheet.src = "Dude_Monster_Idle_4.png"
+        framestotal = 4 
+    }
+
+    if (keys.a) {
+        sp1.xpos -= sp1.speed
+    }
+
+    if (keys.d) {
+        sp1.xpos += sp1.speed
+    }
+
+    if (keys.w) {
+        sp1.ypos -= sp1.speed
+    }
+
+    if (keys.s) {
+        sp1.ypos += sp1.speed
+    }
+
+
+    ctx.drawImage(spritesheet, frameIndex * spritewidth, 0, spritewidth, spriteheight, sp1.xpos, sp1.ypos, spritewidth*5, spriteheight*5
+    )
+
+    frameIndex = (frameIndex + 1) % framestotal
+    requestAnimationFrame(draw)
+}
+
+spritesheet.onload = requestAnimationFrame(draw)

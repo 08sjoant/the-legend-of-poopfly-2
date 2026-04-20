cdbe3508eadf2152e299f8d9ab30253fe460b272
08SjoAnt <anton.sjogren@skola.taby.se>
2026-04-20 11:21:58 +0200

    fiende rörelselogik
    Co-authored-by: ludvigstenlund <ludvigstenlund@users.noreply.github.com>

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ let lastTimestamp = 0,x
 
 
 
+let numberOfProjectiles = 0
+
 class character {
     constructor(name, speed, fire_rate, damage, xpos, ypos) {
         this.name = name;
@@ -30,23 +32,32 @@ class character {
 }
 
 let sp1 = new character("Standardkaraktär", 20, 2, 3, 0, 200)
-let fiende1 = new character("Standardfiende", 10, 3, 3,  0, 800)
+let fi1 = new character("Standardfiende", 15, 3, 3,  300, 400)
+let fi2 = new character("Standardfiende", 15, 3, 3,  300, 500)
+let fi3 = new character("Standardfiende", 15, 3, 3,  300, 600)
+let fi4 = new character("Standardfiende", 15, 3, 3,  300, 700)
+let fi5 = new character("Standardfiende", 15, 3, 3,  300, 800)
+
 
 let keys = {
     w: false,
     s: false,
     d: false,
     a: false,
-    ArrowLeft: false,
-    ArrowRight: false,
     ArrowUp: false,
     ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false,
 }
 
 document.onkeydown = function(e) {
     console.log(e)
     const key = e.key
     keys[key] = true
+    if (e.key == "ArrowUp" || e.key == "ArrowDown" || e.key == "ArrowLeft" || e.key == "ArrowRight"){
+        numberOfProjectiles += 1
+        shoot(e.key)
+    }
 }
 
 document.onkeyup = function(e) {
@@ -56,6 +67,16 @@ document.onkeyup = function(e) {
 }
 
 
+function moveEnemies(fiende) {
+    const dx = sp1.xpos - fiende.xpos;
+    const dy = sp1.ypos - fiende.ypos;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+
+    fiende.xpos = fiende.xpos + (dx / distance) * fiende.speed;
+    fiende.ypos = fiende.ypos + (dy / distance) * fiende.speed;
+
+    ctx.drawImage(spritesheet, frameIndex * spritewidth, 0, spritewidth, spriteheight, fiende.xpos, fiende.ypos, spritewidth, spriteheight)
+}
 
 function draw(timestamp) {
     if (timestamp - lastTimestamp < timestep) {
@@ -68,6 +89,11 @@ function draw(timestamp) {
 
     ctx.clearRect(0, 0, canvas.width, canvas.height,)
 
+    moveEnemies(fi1)
+    moveEnemies(fi2)
+    moveEnemies(fi3)
+    moveEnemies(fi4)
+    moveEnemies(fi5)
 
     if (keys.a || keys.d || keys.w || keys.s) {
         spritesheet.src = "Dude_Monster_Walk_6.png"
@@ -78,33 +104,15 @@ function draw(timestamp) {
         framestotal = 4 
     }
 
-    if (keys.a) {
-        sp1.xpos -= sp1.speed
-    }
+    if (keys.a) {sp1.xpos -= sp1.speed}
+    if (keys.d) {sp1.xpos += sp1.speed}
+    if (keys.w) {sp1.ypos -= sp1.speed}
+    if (keys.s) {sp1.ypos += sp1.speed}
 
-    if (keys.d) {
-        sp1.xpos += sp1.speed
-    }
-
-    if (keys.w) {
-        sp1.ypos -= sp1.speed
-    }
-
-    if (keys.s) {
-        sp1.ypos += sp1.speed
-    }
-
-
-    ctx.drawImage(spritesheet, frameIndex * spritewidth, 0, spritewidth, spriteheight, sp1.xpos, sp1.ypos, spritewidth*5, spriteheight*5
-    )
+    ctx.drawImage(spritesheet, frameIndex * spritewidth, 0, spritewidth, spriteheight, sp1.xpos, sp1.ypos, spritewidth, spriteheight)
 
     frameIndex = (frameIndex + 1) % framestotal
     requestAnimationFrame(draw)
 }
 
 spritesheet.onload = requestAnimationFrame(draw)
-
-function shoot(direction) {
-    if (keys.ArrowRight) {
-    }
-}
